63059c55704aeb19358243364b0384b0cbbda392
tatsato <bboy0801tty@icloud.com>
2025-09-23 14:29:37 +0000

    Redux用の型安全カスタムフックを実装
      - useAppDispatch: AppDispatch型を使用した型安全なdispatchフック
      - useAppSelector: RootState型を使用した型安全なselectorフック
      - 両フックのテストを追加し、動作確認と型安全性を検証
    
      テスト: React Testing Libraryを使用したフック単体テスト

---
+++ app/javascript/hooks/useAppDispatch.ts
@@ -0,0 +1,7 @@
+ // Redux Toolkit の useDispatch を型安全にするカスタムフック
+import { useDispatch } from 'react-redux'
+import type { AppDispatch } from '@/store'
+
+// AppDispatch型を使用して、型安全なdispatchフックを提供
+// これにより、TypeScriptが自動的にアクションの型をチェックする
+export const useAppDispatch = () => useDispatch<AppDispatch>()
